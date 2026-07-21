88aa8c640de761e167b634043f9404e596989ade
Daiss216 <shimulsuplekar21@gmail.com>
2026-07-21 22:48:47 +0530

    Update Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -116,7 +116,7 @@ const Contact = () => {
               </div>
               <div>
                 <h3 className="text-lg font-semibold mb-2">Email</h3>
-                <p className="text-gray-400">shimulsuplekar848@gmail.com</p>
+                <p className="text-gray-400">shimulsuplekar21@gmail.com</p>
               </div>
             </div>
 
